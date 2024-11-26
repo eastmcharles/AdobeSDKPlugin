47724a6449294bf40fa71ee8195fc0e4bf8bb08e
eastmcharles <eastmancharles38@gmail.com>
2024-11-26 06:03:08 -0800

    Add project files.

---
+++ AdobeSDKPlugin/AdobeSDKPluginDlg.h
@@ -0,0 +1,33 @@
+
+// AdobeSDKPluginDlg.h : header file
+//
+
+#pragma once
+
+
+// CAdobeSDKPluginDlg dialog
+class CAdobeSDKPluginDlg : public CDialogEx
+{
+// Construction
+public:
+	CAdobeSDKPluginDlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ADOBESDKPLUGIN_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
